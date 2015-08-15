7be01e586e892e854e4b51dac13f1db86d49be93
王万霖 <dgideas@outlook.com>
2015-08-15 16:48:38 +0800

    build 2.0.13

--- DGStorage.py
+++ DGStorage.py
@@ -12,13 +12,15 @@ except OSError:
 	pass;
 
 class DGStorage:
-	def __init__(self):
+	def __init__(self,conf={}):
 		self.DGSTORAGE_VERSION='2.0';
 		self.DGSTORAGE_CHARSET='utf8';
 		self.DGSTORAGE_SINGLECOLLECTIONLIMIT=1024;
-		
+		self.DGSTORAGE_SEARCHRANGE=3;
+				
 		self.Location='';
 		self.CollectionCache=[];
+		self.LastCollection='';
 	
 	def create(self,name):
 		import codecs;
@@ -33,12 +35,13 @@ class DGStorage:
 		except FileNotFoundError:
 			os.mkdir(str(name));
 			self.Location=name;
-			with codecs.open('conf.dgb','a',self.DGSTORAGE_CHARSET) as conf:
-				conf.write(uuid.uuid1()+'\n');
-				conf.write('version:'+self.DGSTORAGE_VERSION);
-			os.mkdir('index');
-			with codecs.open(self.Location+'/index.dgi','a',self.DGSTORAGE_CHARSET) as index:
+			with codecs.open(self.Location+'/conf.dgb','a',self.DGSTORAGE_CHARSET) as conf:
+				conf.write(str(uuid.uuid1())+'\n');
+				conf.write('Version:'+self.DGSTORAGE_VERSION);
+			os.mkdir(self.Location+'/index');
+			with codecs.open(self.Location+'/index/index.dgi','a',self.DGSTORAGE_CHARSET) as index:
 				pass;
+			os.mkdir(self.Location+'/cache')
 			return True;
 		else:
 			return False;
@@ -59,16 +62,16 @@ class DGStorage:
 			with open('conf.dgb') as conf:
 				correctVersion=False;
 				for line in conf:
-					if line.find('version:2')!=-1:
+					if line.find('Version:2')!=-1:
 						correctVersion=True;
 				if correctVersion==False:
 					return False;
-			with open(self.Location+'/index.dgi') as index:
+			with open('index/index.dgi') as index:
 				for line in index:
 					line=line.replace('\n','');
 					if line!='' and line!='\n':
 						self.CollectionCache.append(line);
-			return True;
+			return len(CollectionCache);
 	
 	def add(self,key,content,prop={}):
 		import codecs;
@@ -77,7 +80,7 @@ class DGStorage:
 		import os;
 		import sys;
 		
-		key.replace('\n','');
+		key=str(key).replace('\n','');
 		key=urllib.parse.quote_plus(str(key));
 		operationCollection=''
 		if key=='':
@@ -88,61 +91,88 @@ class DGStorage:
 			else:
 				return False;
 		else:
-			lastCollection='';
-			for collection in self.CollectionCache:
-				lastCollection=collection;
-				with open(self.Location+'/'+str(collection)+'/index/index.dgc') as collIndex:
+			if self.LastCollection!='':
+				with open(str(self.LastCollection)+'/index/index.dgi') as collIndex:
 					i=0;
 					for line in collIndex:
 						if line!='':
 							i+=1;
 					if i<self.DGSTORAGE_SINGLECOLLECTIONLIMIT:
-						operationCollection=collection;
-						break;
+						operationCollection=self.LastCollection;
 					else:
-						continue;
-			if operationCollection=='':
-				self.createcoll(int(lastCollection)+1);
-				operationCollection=int(lastCollection)+1;
+						operationCollection=self.findavailablecoll(True);
+			else:
+				operationCollection=self.findavailablecoll(True);
+		self.LastCollection=operationCollection;
 		uid='';
-		with codecs.open(self.Location+'/'+str(operationCollection)+'/index/index.dgc','a','utf8') as collindex:
-			collindexR=open(self.Location+'/'+str(operationCollection)+'/index/index.dgc');
+		with codecs.open(str(operationCollection)+'/index/index.dgi','a','utf8') as collIndex:
+			collIndexR=open(str(operationCollection)+'/index/index.dgi');
 			i=0;
-			for line in collindexR:
+			for line in collIndexR:
 				if line!='' and line!='\n':
 					i+=1;
-			collindexR.close();
+			collIndexR.close();
 			uid=uuid.uuid1();
 			if i==0:
-				collindex.write(str(uid));
+				collIndex.write(str(uid)+','+str(key));
 			else:
-				collindex.write('\n'+str(uid));
-		with codecs.open(self.Location+'/'+str(operationCollection)+'/'+str(uid)+'.dgs','a','utf8') as storage:
-			storage.write(content);
-		with codecs.open(self.Location+'/'+str(operationCollection)+'/'+str(uid)+'.dgp','a','utf8') as storageProp:
-			for propItem in prop:
-				storageProp.write(str(propItem)+'\n');
-		return True;
-					
+				collIndex.write('\n'+str(uid)+','+str(key));
+		with codecs.open(str(operationCollection)+'/'+str(uid)+'.dgs','a','utf8') as storage:
+			storage.write(str(content));
+		if len(prop)!=0:
+			with codecs.open(str(operationCollection)+'/'+str(uid)+'.dgp','a','utf8') as storageProp:
+				for propItem in prop:
+					storageProp.write(str(propItem)+':'+str(prop[propItem])+'\n');
+		return uid;
+	
+	def get(self,key,limit=-1):
+		if limit==0:
+			return False;
+		
+		
 	
 	#Private
 	def clche(self,where=''):
-		if where='':
+		if where=='':
 			self.CollectionCache=[];
 	
 	def createcoll(self,coll):
+		import codecs;
+		import os;
 		try:
 			os.mkdir(str(coll));
 		except FileExistsError:
 			return False;
 		else:
 			os.mkdir(str(coll)+'/index');
-			with codecs.open(str(coll)+'/index/index.dgc','a','utf8') as dgc:
+			with codecs.open(str(coll)+'/index/index.dgi','a','utf8') as dgc:
 				pass;
 			self.CollectionCache.append(coll);
-			with open(self.Location+'/index/index.dgi','a') as index:
-				index.write(coll+'\n');
+			with open('index/index.dgi','a') as index:
+				index.write(str(coll)+'\n');
 			return True;
 	
 	def removecoll(self,coll):
-		
+		return;
+	
+	def findavailablecoll(self,createNewColl=False):
+		searchRange=self.DGSTORAGE_SEARCHRANGE;
+		if searchRange!='' or searchRange!=None:
+			searchRange=-1-int(searchRange);
+		for collection in self.CollectionCache[:searchRange:-1]:
+			with open(str(collection)+'/index/index.dgi') as collIndex:
+				i=0;
+				for line in collIndex:
+					if line!='':
+						i+=1;
+				if i<self.DGSTORAGE_SINGLECOLLECTIONLIMIT:
+					return collection;
+					break;
+				else:
+					continue;
+		if createNewColl==True:
+			self.createcoll(int(self.LastCollection)+1);
+			return int(self.LastCollection)+1;
+		else:
+			return False;
+	
