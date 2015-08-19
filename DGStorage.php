1cc55ec85ef4f2f0971913dc33413e3a87f78b68
王万霖 <dgideas@outlook.com>
2015-08-19 19:18:05 +0800

    Update DGStorage.php

--- DGStorage.php
+++ DGStorage.php
@@ -1,12 +1,21 @@
 <?php
-    function CallShell($action=fetch,$datacluster=db,$value=""){
-    //exec("python DGStorage.py fetch textstat 20 0 out.php");
-    exec("python DGStorage.py".$action." ".$datacluster." ".$value);
-					$res=file_get_contents('out.php');
-					$res=explode("\n",$res);
-					foreach ($res as &$item){
-						$array=str_getcsv($item);
-						
-					}
-    }
+	function DGContent($action=fetch,$datacluster=db,$value="",$outfile="out.php"){
+		exec("python DGStorage.py".$action." ".$datacluster." ".$value);
+		$res=file_get_contents($outfile);
+		$res=explode("\n",$res);
+		foreach ($res as &$item){
+			$array=explode(",",$item,3);
+			return urldecode($array[1]);
+	}
+    function DGProp($action=fetch,$datacluster=db,$value="",$outfile="out.php"){
+				exec("python DGStorage.py".$action." ".$datacluster." ".$value);
+				$res=file_get_contents($outfile);
+				$res=explode("\n",$res);
+				foreach ($res as &$item){
+					$array=explode(",",$item,3);
+					$array[2]=str_replace("'",'"',$array[2]);
+					$array[2]=str_replace('"{','{',$array[2]);
+					$array[2]=str_replace('}"','}',$array[2]);
+					return json_decode($array[2]);
+	}}
 ?>
