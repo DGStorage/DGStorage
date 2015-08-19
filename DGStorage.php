8434858b2d0165e87621c5450e0599764a87c7b8
王万霖 <dgideas@outlook.com>
2015-08-19 18:52:19 +0800

    Update DGStorage.php

--- DGStorage.php
+++ DGStorage.php
@@ -1,3 +1,12 @@
 <?php
-//先占坑，以后再说
+    function CallShell($action=fetch,$datacluster=db,$value=""){
+    //exec("python DGStorage.py fetch textstat 20 0 out.php");
+    exec("python DGStorage.py".$action." ".$datacluster." ".$value);
+					$res=file_get_contents('out.php');
+					$res=explode("\n",$res);
+					foreach ($res as &$item){
+						$array=str_getcsv($item);
+						
+					}
+    }
 ?>
