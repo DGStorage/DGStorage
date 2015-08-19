d928fb158df92a5ffca4d8a3aa33970b190aa436
王万霖 <dgideas@outlook.com>
2015-08-19 19:20:14 +0800

    fix major bug

--- DGStorage.php
+++ DGStorage.php
@@ -6,7 +6,7 @@
 		foreach ($res as &$item){
 			$array=explode(",",$item,3);
 			return urldecode($array[1]);
-	}
+	}}
     function DGProp($action=fetch,$datacluster=db,$value="",$outfile="out.php"){
 				exec("python DGStorage.py".$action." ".$datacluster." ".$value);
 				$res=file_get_contents($outfile);
