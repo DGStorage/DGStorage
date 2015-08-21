2b5388995f3a0fe1e6bc4bc154c268183847d119
王万霖 <dgideas@outlook.com>
2015-08-21 12:10:20 +0800

    minor change

--- DGStorage.php
+++ DGStorage.php
@@ -1,34 +1,6 @@
 <?php
 	//Designed for PHP5
 	//Thanks for boxfish education's help
-	
-	function DGContent($action="fetch",$datacluster="db",$value="",$outfile="out.php")
-	{
-		exec("python DGStorage.py".$action." ".$datacluster." ".$value." ".$outfile);
-		$res=file_get_contents($outfile);
-		$res=explode("\n",$res);
-		foreach ($res as &$item)
-		{
-			$array=explode(",",$item,3);
-			return urldecode($array[1]);
-		}
-	}
-	function DGProp($action="fetch",$datacluster="db",$value="",$outfile="out.php")
-	{
-		exec("python DGStorage.py".$action." ".$datacluster." ".$value." ".$outfile);
-		$res=file_get_contents($outfile);
-		$res=explode("\n",$res);
-		foreach ($res as &$item)
-		{
-			$array=explode(",",$item,3);
-			$array[2]=str_replace("'",'"',$array[2]);
-			$array[2]=str_replace('"{','{',$array[2]);
-			$array[2]=str_replace('}"','}',$array[2]);
-			return json_decode($array[2]);
-		}
-	}
-	
-	
 	class DGStorage
 	{	
 	
