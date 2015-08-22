743355a748cbd1190d250633ea08a53de4076890
王万霖 <dgideas@outlook.com>
2015-08-22 16:11:18 +0800

    minor changes

--- DGStorage.js
+++ DGStorage.js
@@ -1,1 +1,42 @@
-<!---->
+//person.js
+var num = 0;
+
+exports.Person = function(pName){
+
+   //private
+
+   var pri = {
+
+       pName : "",
+
+       pAge : 0
+
+   }
+
+   //public
+
+   var pub = {
+
+       setName : function(pName){
+
+           pri.pName = pName;
+
+       },
+
+       getName : function(){
+
+           return pri.pName;
+
+       }
+
+   }
+
+//construct code
+
+   pri.pName = pName;
+
+
+
+   return pub;
+
+}
